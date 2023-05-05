963dbea760538eb1d6da4cbe570cee8c008cbab4
ayman toumi <aymantoumi99@gmail.com>
2023-05-05 15:33:40 +0100

    Home Layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,21 @@
 @extends('layouts.main')
 
 @section('Content')
-    <section>
-        hello mom
+    <section class="home-grid home-color">
+        <div class="total-patients">
+            <h2>Total</h2>
+        </div>
+        <div class="patients-waiting">
+            <h2>Waiting</h2>
+        </div>
+        <div class="patients-done">
+            <h2>Done</h2>
+        </div>
+        <div class="patients-waiting-list">
+            <h2>List</h2>
+        </div>
+        <div class="patients-form">
+            <h2>Form</h2>
+        </div>
     </section>
 @endsection
