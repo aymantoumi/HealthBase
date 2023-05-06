6b26a05504a419e591a8d6fc0c3f11bda33a0353
ayman toumi <aymantoumi99@gmail.com>
2023-05-06 02:34:50 +0100

    Home Page Working on

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,69 @@
 
 @section('Content')
     <section class="home-grid home-color">
-        <div class="total-patients">
-            <h2>Total</h2>
+        <div class="total-patients shadow">
+            <div class="icon">
+
+            </div>
+            <div class="title">
+
+            </div>
+            <div class="value">
+
+            </div>
         </div>
-        <div class="patients-waiting">
-            <h2>Waiting</h2>
+        <div class="patients-waiting shadow">
+            <div class="icon">
+
+            </div>
+            <div class="title">
+
+            </div>
+            <div class="value">
+
+            </div>
         </div>
-        <div class="patients-done">
-            <h2>Done</h2>
+        <div class="patients-done shadow">
+            <div class="icon">
+
+            </div>
+            <div class="title">
+
+            </div>
+            <div class="value">
+
+            </div>
         </div>
-        <div class="patients-waiting-list">
-            <h2>List</h2>
+        <div class="patients-waiting-list shadow">
+
         </div>
-        <div class="patients-form">
-            <h2>Form</h2>
+        <div class="patients-form shadow">
+            <form action="" method="get">
+                <div>
+                    <input type="text" name="" id="">
+                    <input type="text" name="" id="">
+                </div>
+                <div>
+                    <input type="text" name="" id="">
+                    <input type="text" name="" id="">
+                </div>
+                <div>
+                    <input type="text" name="" id="">
+                    <input type="text" name="" id="">
+                </div>
+                <div>
+                    <input type="text" name="" id="">
+                    <input type="text" name="" id="">
+                </div>
+                <div>
+                    <input type="text" name="" id="">
+                    <input type="text" name="" id="">
+                </div>
+                <div >
+                    <button class="button green">Submit</button>
+                    <button class="button red">Camsel</button>
+                </div>
+            </form>
         </div>
     </section>
 @endsection
